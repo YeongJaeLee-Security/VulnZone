aa6964327ee6e038bcd3c616e3400e51771a64ef
YeongJaeLee-Security <dudwo8528@gmail.com>
2024-12-09 21:09:21 +0900

    회원 DataBase 구축 & 로그인 기능 구현

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -1,26 +1,54 @@
 package user;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.*;
 
 public class UserDAO {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
+	private Properties properties = new Properties();
 	
 //	생성자 - UserDAO 객체 인스턴스를 생성할 때 자동으로 DB 연결
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/VulnZone";
-			String dbID = "root";
-			String dbPassword = "password";
-			Class.forName("com.mysql.jdbc.Driver");
+			InputStream inputStream = UserDAO.class.getClassLoader().getResourceAsStream("config.properties");
+			properties.load(inputStream);
+			
+			String dbURL = properties.getProperty("db.url");
+			String dbID = properties.getProperty("db.id");
+			String dbPassword = properties.getProperty("db.password");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String sql = "SELECT userPassword FROM USER WHERE userID = ?";
+		
+		try {
+			// SQL injection 방지
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				// 1 - 로그인 성공, 0 - 비밀번호 불일치
+				return rs.getString(1).equals(userPassword) ? 1 : 0;
+			}
+			
+			return -1;	// 아이디 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return -2;	// 데이터베이스 오류
 	}
 }

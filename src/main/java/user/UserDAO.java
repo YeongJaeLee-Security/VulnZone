19a8f5642c7c926ae7d031c0c65d2e5f10f3dac9
YeongJaeLee-Security <dudwo8528@gmail.com>
2024-12-12 22:26:51 +0900

    BruteForce 초기 완성

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 public class UserDAO {
@@ -52,6 +53,14 @@ public class UserDAO {
             return -1; // 아이디 없음
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+        	try {
+	        	if (rs != null) rs.close();
+	        	if (pstmt != null) pstmt.close();
+	        	if (conn != null) conn.close();
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}
         }
 
         return -2; // 데이터베이스 오류
@@ -92,6 +101,14 @@ public class UserDAO {
             return pstmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+        	try {
+	        	if (rs != null) rs.close();
+	        	if (pstmt != null) pstmt.close();
+	        	if (conn != null) conn.close();
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}
         }
 
         return -1; // 데이터베이스 오류
